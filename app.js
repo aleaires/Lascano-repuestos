f9f8662e13ad46f7b9a847e10098323ed40e242d
aleaires <72895379+aleaires@users.noreply.github.com>
2022-06-21 21:38:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+ const app = $("#app");
+
+
+ console.log(app);
+ app.html( `
+ <h1>Pagina en construccion</h1>
+ <p>Seguiremos trabajando para su correcta construccion</p>
+ <p>gracias por su colaboracion</p>
+ `);
