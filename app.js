3decd07c61fc0dbab4391bdd26d218af7729c05e
aleaires <72895379+aleaires@users.noreply.github.com>
2024-06-26 13:56:39 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
- const app = $("#app");
-
-
- console.log(app);
- app.html( `
- <h1>Pagina en construccion</h1>
- <p>Seguiremos trabajando para su correcta construccion</p>
- <p>gracias por su colaboracion</p>
- `);
